7b6137bd0d3126632faaccc1e1b8de78aa4e069d
Derek Hansen <derek.hansen@chiefind.com>
2018-01-29 08:54:32 -0600

    S03.01 changes.

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,64 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by hansend on 1/26/2018.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.forecate_list_item;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = layoutInflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null) {
+            return 0;
+        }
+
+        return mWeatherData.length;
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+
+        void bind(int listIndex) {
+            mWeatherTextView.setText(String.valueOf(listIndex));
+        }
+    }
+}
