4c7feda9b23c52b0ad438dc9a5ee31849b21f88c
Derek Hansen <derek.hansen@chiefind.com>
2018-01-29 14:43:56 -0600

    S04.01 changes.

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -12,6 +12,16 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
     }
 }
